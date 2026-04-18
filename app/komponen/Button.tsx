c198d9aed19c2138363bc2e1f6f0f9c87bca6fa9
maszhar <fikrimustofa024@gmail.com>
2026-04-18 11:43:26 +0700

    fitur: tambah atribut disable pada button

--- app/komponen/Button.tsx
+++ app/komponen/Button.tsx
@@ -7,6 +7,7 @@ interface ButtonProps {
   className?: string;
   onClick?: () => void;
   type?: "button" | "submit";
+  disabled?: boolean;
 }
 
 export function Button({
@@ -16,28 +17,24 @@ export function Button({
   href,
   onClick,
   type = "button",
+  disabled = false,
 }: ButtonProps) {
+  const baseClassName =
+    "inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-blue-600 px-6 py-3 text-white font-medium shadow hover:bg-blue-700 transition disabled:bg-blue-100 disabled:text-blue-50 disabled:cursor-default ";
+
   return (
     <>
       {href ? (
-        <NavLink
-          to={href}
-          className={
-            "inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-blue-600 px-6 py-3 text-white font-medium shadow hover:bg-blue-700 transition " +
-            className
-          }
-        >
+        <NavLink to={href} className={baseClassName + className}>
           {leftIcon && leftIcon}
           {children && children}
         </NavLink>
       ) : (
         <button
           onClick={onClick}
-          className={
-            "inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg bg-blue-600 px-6 py-3 text-white font-medium shadow hover:bg-blue-700 transition " +
-            className
-          }
+          className={baseClassName + className}
           type={type}
+          disabled={disabled}
         >
           {leftIcon && leftIcon}
           {children && children}
